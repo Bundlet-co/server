4acced04ac73b0986960cb9dbd5d3d1def4814b4
dev_apus <folorunsopraise580@gmail.com>
2024-09-24 07:54:56 +0100

    Server code

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,7 +2,9 @@ const allowedOrigin = [
       "http://localhost:3500",
       "http://localhost:5173",
       "https://9c39rdqg-5173.uks1.devtunnels.ms",
-      "https://9c39rdqg-3500.uks1.devtunnels.ms"
+      "https://9c39rdqg-3500.uks1.devtunnels.ms",
+      process.env.FRONTEND_URL,
+      process.env.ADMIN_FRONTEND_URL 
 ];
 
 module.exports = allowedOrigin;
